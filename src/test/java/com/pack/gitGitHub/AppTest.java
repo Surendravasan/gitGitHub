165d6ce41fe3ab2d053bd992304cae9249c1de09
Anand <Anand@Anand-TOSH>
2019-12-23 10:55:35 +0530

    Removed unwanted class and change in log text

--- src/test/java/com/pack/gitGitHub/AppTest.java
+++ src/test/java/com/pack/gitGitHub/AppTest.java
@@ -4,7 +4,8 @@ public class AppTest extends App {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Main Test Method/");
+		System.out.println("Main Test Method");
+		System.out.println("Main Test Method 2");
 	}
 	
 	
